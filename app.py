bba8c45e52a7323b26a047764884a8e7158aa8cd
Bunny <bunnygamingowo@gmail.com>
2025-12-30 13:29:00 +0530

    Trying to deploy

--- app.py
+++ app.py
@@ -88,8 +88,7 @@ def logout():
     session.clear()
     return redirect("/login")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__=="__main__":
+	app.run(host="0.0.0.0",port=5000)
 
 
